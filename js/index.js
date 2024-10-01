8419e9805299857b0114a936085dc58200a4ccc0
Paterne <diedhioupaterne279@gmail.com>
2024-10-01 14:05:57 +0000

    amelioration de ma fonction addLivre

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-API_KEY = "API_KEY=4e9f68c76b4e3d7a8b5f1c2d3e4f5a6b";
+API_KEY = "4e9f68c76b4e3d7a8b5f1c2d3e4f5a6b";
 API = "http://localhost/e-bestcommerce_mudey/backend_personnel/api/";
 
 
@@ -15,25 +15,25 @@ openModale = ()  =>{
 
 
 addLivre = () => {
-    const newProduct = {
-        titre: localStorage.getItem("titre"),
-        autheur: localStorage.getItem("autheur"),
-        isbn: localStorage.getItem("isbn"),
-        datePub: localStorage.getItem("datePub"),
-        disponibilite: localStorage.getItem("disponibilite")
-    }
+    //Amelioration de ma fonction add Livre
+    let formLivre = document.getElementById("formLivre");
+
+    let data = new FormData(formLivre);
+    data.append("API_KEY", API_KEY);
 
     //nettoyer le localStorage
     localStorage.clear();
 
     //creation de l'url pour la sauvegarde en bd
-    let params = constructURLParams(newProduct);
-    const url = API + 'livre?' + params + API_KEY;
+    const url = API + 'livre';
 
     //console.log(url);
 
-    fetch(url, {method: "POST"})
-        .then((response) => { //un then pour recuperer la response
+    fetch(url, 
+        {
+            method: "POST",
+            body: data
+        }).then((response) => { //un then pour recuperer la response
             if (response.ok) {
                 return response.json();
             }
@@ -41,6 +41,7 @@ addLivre = () => {
         .then((response) => { //un autre then pour recuperer la response dans ce format
             if (response.status == 200) {
                 console.log(response.result);
+                document.getElementById("formLivre").reset(); //remettre à Zero tous les champs
                 document.getElementsByClassName("btn-close")[0].click(); //fermer le modal
 
             }else{
