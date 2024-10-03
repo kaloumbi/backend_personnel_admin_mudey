9e8244d10200232a8f32a1a5853691e11b897188
Paterne <diedhioupaterne279@gmail.com>
2024-10-03 15:14:09 +0000

    recuperation des valeurs pour la mise à jour

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+
 API_KEY = "4e9f68c76b4e3d7a8b5f1c2d3e4f5a6b";
 API = "http://localhost/e-bestcommerce_mudey/backend_personnel/api/";
+
 
 
 openModale = ()  =>{
@@ -13,7 +15,52 @@ openModale = ()  =>{
 
 }
 
+//Ouverture du modal pour l'ajout de l'emprunt(c'est particulier) !
+openModaleEmpruntAdd = ()  =>{
+    console.log(`addProd`, 'azertyui'); 
 
+   
+
+    var myModal = new bootstrap.Modal($('#exampleModal'), {
+        backdrop: true, // ou true/false selon vos besoins
+        keyboard: false
+    })
+    console.log(`modal`, myModal);
+    myModal.show()
+
+}
+
+/******************
+                    FONCTIONS UPDATED 
+*******************/
+//modal Livre 
+openModaleUpdateLivre = (id)  =>{
+    console.log(`Opening update modal for product ID: ${id}`);
+    var myModal = new bootstrap.Modal($(`#updateLivre-${id}`), {
+        backdrop: true, // ou true/false selon vos besoins
+        keyboard: false
+    })
+    console.log(`modal`, myModal);
+    myModal.show()
+
+}
+
+
+//modal User
+openModaleUpdateUser = (id)  =>{
+    console.log(`Opening update modal for product ID: ${id}`);
+    var myModal = new bootstrap.Modal($(`#updateUser-${id}`), {
+        backdrop: true, // ou true/false selon vos besoins
+        keyboard: false
+    })
+    console.log(`modal`, myModal);
+    myModal.show()
+
+}
+
+/****************************
+            AJOUT LIVRE, USER, EMPRUNT
+****************************/
 addLivre = () => {
     //Amelioration de ma fonction add Livre
     let formLivre = document.getElementById("formLivre");
@@ -42,6 +89,96 @@ addLivre = () => {
             if (response.status == 200) {
                 console.log(response.result);
                 document.getElementById("formLivre").reset(); //remettre à Zero tous les champs
+                document.getElementsByClassName("btn-close")[0].click(); //fermer le modal
+
+            }else{
+                console.log(response.message);
+            
+            }
+        })
+    
+    
+    // console.log("Mon Produit affiché", constructURLParams(newProduct));
+    
+}
+
+
+//Ajout User
+addUser = () => {
+    //Amelioration de ma fonction add Livre
+    let formUser = document.getElementById("formUser");
+
+    let data = new FormData(formUser);
+    data.append("API_KEY", API_KEY);
+
+    //nettoyer le localStorage
+    localStorage.clear();
+
+    //creation de l'url pour la sauvegarde en bd
+    const url = API + 'user';
+
+    //console.log(url);
+
+    fetch(url, 
+        {
+            method: "POST",
+            body: data
+        }).then((response) => { //un then pour recuperer la response
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then((response) => { //un autre then pour recuperer la response dans ce format
+            if (response.status == 200) {
+                console.log(response.result);
+                document.getElementById("formUser").reset(); //remettre à Zero tous les champs
+                document.getElementsByClassName("btn-close")[0].click(); //fermer le modal
+
+            }else{
+                console.log(response.message);
+            
+            }
+        })
+    
+    
+    // console.log("Mon Produit affiché", constructURLParams(newProduct));
+    
+}
+
+// Ajout Emprunt
+
+
+
+addEmprunt = () => {
+    //Amelioration de ma fonction add Livre
+    let formEmprunt = document.getElementById("formEmprunt");
+
+    let data = new FormData(formEmprunt);
+    data.append("API_KEY", API_KEY);
+
+    //nettoyer le localStorage
+    localStorage.clear();
+
+    //creation de l'url pour la sauvegarde en bd
+    const url = API + 'emprunt';
+
+    //console.log(url);
+
+    fetch(url, 
+        {
+            method: "POST",
+            body: data
+        }).then((response) => { //un then pour recuperer la response
+            if (response.ok) {
+                return response.json();
+            }else {
+                throw new Error("Erreur lors de l'envoi des données.");
+            }
+        })
+        .then((response) => { //un autre then pour recuperer la response dans ce format
+            if (response.status == 200) {
+                console.log(response.result);
+                document.getElementById("formEmprunt").reset(); //remettre à Zero tous les champs
                 document.getElementsByClassName("btn-close")[0].click(); //fermer le modal
 
             }else{
--- js/library.js
+++ js/library.js
@@ -98,11 +98,113 @@ class Library {
                 { data: 'isbn' },
                 { data: 'datePub.date' },
                 { data: 'disponibilite' },
-                { data: 'createdAt.date' }
+                { data: 'createdAt.date' },
+                { data: 'id',
+                    render: function ( id, type, row ) {
+                       return `<button type="button" class="btn btn-success" data-toggle="modal" data-target="#updateLivre-${id}" onclick="openModaleUpdateLivre(${id})"> UPDATE </button> 
+                       
+                            <!-- Modal -->
+                            <div class="modal" id="updateLivre-${id}" tabindex="-1" aria-labelledby="updateLivreModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Update Livre</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">x</button>
+                                    </div>
+                                    <div class="modal-body">
+                                    
+                                    <form action="" id="formUpdateLivre-${id}">
+                                        <div class="form-row">
+                                            <div class="col">
+                                                <label for="">Titre : </label>
+                                                <input  type="text" name="titre" class="form-control" value="${row.titre}" >
+                                            </div>
+                            
+                            
+                                            <div class="col">
+                                                <label for="">Autheur : </label>
+                                                <input  type="text" name="autheur" class="form-control" value="${row.autheur}" >
+                                            </div>
+                            
+                            
+                                            <div class="col">
+                                                <label for="">ISBN : </label>
+                                                <input  type="text" name="isbn" class="form-control" value="${row.isbn}" >
+                                            </div>
+                            
+                            
+                                            <div class="col">
+                                                <label for="">Date Pub :</label>
+                                                <input  type="date" name="datePub" class="form-control" value="${new Date(row.datePub.date).toISOString().substring(0, 10)}" >
+                                            </div>
+                                            
+                            
+                            
+                                            <div class="col">
+                                                <label for="">Disponibilité :</label><br>
+                                                <input  type="radio" name="disponibilite" value="true" ${row.disponibilite ? 'checked' : ''} > Oui
+                                                <input  type="radio" name="disponibilite" value="false" ${!row.disponibilite ? 'checked' : ''} > Non
+                                            </div>
+                                            
+                            
+                            
+                                        </div>
+                                    </form>
+                                    
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button onclick="addLivre(${id})" type="button" class="btn btn-primary">Update Livre</button>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                       
+                       
+                       `;
+                    }
+                }
+                
             ]
         });
     }
-        
+
+
+    /************
+     * ************ FONCTION POUR CHARGER LES USERS ET LIVRES AVEC LES OPTIONS ******************
+     ************/
+
+    //Fonction pour charger les utilisateurs dans le <select> :
+    loadUserOptions() {
+        let userSelect = document.getElementById('userSelect');
+        userSelect.innerHTML = ""; // Vider les options existantes
+    
+        const users = this.getData('users'); // Récupérer les utilisateurs
+    
+        users.forEach(user => {
+            let option = document.createElement('option');
+            option.value = user.id; // L'ID de l'utilisateur
+            option.text = `${user.prenom} ${user.nom}`; // Nom complet de l'utilisateur
+            userSelect.appendChild(option); // Ajouter l'option au select
+        });
+    }
+
+    //Fonction pour charger les livres dans le <select> :
+    loadLivreOptions() {
+        let livreSelect = document.getElementById('livreSelect');
+        livreSelect.innerHTML = ""; // Vider les options existantes
+    
+        const livres = this.getData('livres'); // Récupérer les livres
+    
+        livres.forEach(livre => {
+            let option = document.createElement('option');
+            option.value = livre.id; // L'ID du livre
+            option.text = livre.titre; // Titre du livre
+            livreSelect.appendChild(option); // Ajouter l'option au select
+        });
+    }
+    
+    
 
     loadEmprunts(){
         // const data = this.getData('emprunts');
@@ -143,7 +245,56 @@ class Library {
                 { data: 'id' },
                 { data: 'prenom' },
                 { data: 'nom' },
-                { data: 'login' }
+                { data: 'login' },
+                { data: 'id', 
+                    render: function ( id, type, row ) {
+                        return `<button type="button" class="btn btn-success" data-toggle="modal" data-target="#updateUser-${id}" onclick="openModaleUpdateUser(${id})"> UPDATE </button>
+                            <!-- Modal -->
+                            <div class="modal" id="updateUser-${id}" tabindex="-1" aria-labelledby="updateUserModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Update User</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">x</button>
+                                    </div>
+                                    <div class="modal-body">
+                                    
+                                    <form action="" id="formUpdateUser">
+                                        <div class="form-row">
+                                            <div class="col">
+                                                <label for="">Prenom : </label>
+                                                <input  type="text" name="prenom" class="form-control" value="${row.prenom}" >
+                                            </div>
+                            
+                            
+                                            <div class="col">
+                                                <label for="">Nom : </label>
+                                                <input  type="text" name="nom" class="form-control" value="${row.nom}" >
+                                            </div>
+                            
+                            
+                                            <div class="col">
+                                                <label for="">Login : </label>
+                                                <input  type="text" name="login" class="form-control" value="${row.login}" >
+                            
+                                            </div>
+                            
+                                        </div>
+                                    </form>
+                                    
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button onclick="updateUser(${id})" type="button" class="btn btn-primary">Update User</button>
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+
+                        `;
+                    } 
+                }
             ]
         } );
     }
